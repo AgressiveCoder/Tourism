5c60bea2ceafaa9587add03092676af36be6dae8
AgressiveCoder <85111739+AgressiveCoder@users.noreply.github.com>
2022-05-29 19:36:29 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
+
+window.onload = function () {
+  document.body.classList.add('loaded');
+}
+
+
 $(document).ready(function(){
-  $('.main-carousel').slick({
-    setting-name: setting-value
-  });
+$('.main-carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 3
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
+  }
+]
+});
 });
